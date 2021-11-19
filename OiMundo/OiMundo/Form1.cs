f57f65651fe5a2c0207ad8b8854d097150ad932d
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-18 22:20:42 -0300

    Estudos de C# - Criação de variaveis e Forms

---
+++ OiMundo/OiMundo/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OiMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string mensagem = "Olá";
+            string nome = "Jose";
+            MessageBox.Show($"{mensagem} {nome}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int idadeJoao = 10;
+            int idadeMaria = 25;
+            int idadeJose = 30;
+            int mediaIdades = (idadeJoao + idadeMaria + idadeJose) / 3;
+
+            MessageBox.Show("Media das idades " + mediaIdades);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //int pi = 3.14; // O codigo não compila
+            
+            //MessageBox.Show("pi " + pi);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double pi = 3.14;
+            int piQuebrado = (int)pi;
+
+            MessageBox.Show("piQuebrodo = " + piQuebrado);
+        }
+    }
+}
