255d99828c3b99df2aa05fe403b98e3cd097721b
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-21 23:54:05 -0300

    Loop usando while e do while

---
+++ LoopAndCSharp/LoopAndCSharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopAndCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Loop - While
+            /*int a = 1;
+            while (a <= 10)
+            {
+                if (a == 1)
+                {
+                    a++;
+                    continue;
+                }
+                Console.WriteLine(a);
+                a++;
+            }*/
+
+            //Loop - Do While
+            int a = 1;
+
+            do
+            {
+                Console.WriteLine(a);
+                a++;
+            } while (a < 3);
+
+        }
+    }
+}
