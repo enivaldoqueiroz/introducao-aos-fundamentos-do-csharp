3dfcba739c38549c946ec8e962cf172bad130078
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-23 19:55:46 -0300

    Conversao Implicita e casting

---
+++ Aula20ConversaoImplicitaECasting/Aula20ConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20ConversaoImplicitaECasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão implicita entre tipos
+
+            float x = 4.5f;
+            double y = x;
+            Console.WriteLine(y);
+
+            //Casting: conversao explicita entre tipos COMPATIVEIS
+            double a = 5.1;
+            float b = (float)a;
+            Console.WriteLine(b);
+            Console.WriteLine("");
+
+            int a1 = 5;
+            int b1 = 2;
+            Console.WriteLine((double)a1 / b1);
+
+            Console.Read();
+        }
+    }
+}
