63f71103fea7ef931f29a74f5a60dc20bdc8c03e
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-23 15:11:41 -0300

    Aula 18 incompleta

---
+++ Aula18SaidaDeDados/Aula18SaidaDeDados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula18SaidaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 32;
+            double saldo = 10.35784;
+            string nome = "Maria";
+
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo);
+            Console.WriteLine(nome);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+            int idade2 = 32;
+            string saldo2 = 10.35784;
+            string nome2
+
+            Console.Read();
+        }
+    }
+}
