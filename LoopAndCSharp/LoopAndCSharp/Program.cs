d35f6af4d8dcbd4c0ecaeff3c2e9bbbbcc15f6ef
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-22 22:51:50 -0300

    Loop com for, foreach e função recursiva.

--- LoopAndCSharp/LoopAndCSharp/Program.cs
+++ LoopAndCSharp/LoopAndCSharp/Program.cs
@@ -24,14 +24,47 @@ namespace LoopAndCSharp
             }*/
 
             //Loop - Do While
-            int a = 1;
+            /*int a = 1;
 
             do
             {
                 Console.WriteLine(a);
                 a++;
-            } while (a < 3);
+            } while (a < 3);*/
+
+            //Loop - For
+            /*for (int a = 1; a <= 10; a++)
+            {
+                Console.WriteLine(a);
+            }*/
+
+            //Loop - Foreach
+            /*int[] a = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            foreach (var item in a)
+            {
+                Console.WriteLine(item);
+            }
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(a[i]);
+            }*/
+
+            //Loop - com função recursiva
+            looping(1);
+
+            Console.ReadLine();
 
         }
+        //Loop - com função recursiva
+        public static void looping(int a)
+        {
+            Console.WriteLine(a);
+            if(a < 3)
+            {
+                looping(a + 1);
+            }
+        }
+
     }
 }
