c331e03ed012a1a0909fd34b35752de95ebaa847
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-23 10:23:00 -0300

    Aula 10 - Criando projeto no Visual Studio

---
+++ Aula10PrimeiroProjeto/Aula10PrimeiroProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word");
+            Console.Read();
+        }
+    }
+}
