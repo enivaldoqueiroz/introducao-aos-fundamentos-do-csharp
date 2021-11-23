b355b7dfc683fccc1724857938dc35ddb585b884
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2021-11-23 19:18:36 -0300

    Implementação final e exercicio de fixação.

--- Aula18SaidaDeDados/Aula18SaidaDeDados/Program.cs
+++ Aula18SaidaDeDados/Aula18SaidaDeDados/Program.cs
@@ -25,8 +25,17 @@ namespace Aula18SaidaDeDados
             Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
 
             int idade2 = 32;
-            string saldo2 = 10.35784;
-            string nome2
+            double saldo2 = 10.35784;
+            string nome2 = "Maria";
+
+            //PlaceHolder
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome2, idade2, saldo2);
+
+            //Interpolação
+            Console.WriteLine($"{nome2} tem {idade2} anos e tem saldo igual a {saldo2:F2} reais");
+
+            //Concatenação de Strings
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
 
             Console.Read();
         }
