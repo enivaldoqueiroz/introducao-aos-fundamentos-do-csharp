3f7e754e2456a8ed4f13061f99776e8365ac3b16
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-24 11:29:08 -0300

    Estrutura de repetição while

---
+++ Aula32EstruturaDeRepeticaoWhile/Aula32EstruturaDeRepeticaoWhile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula32EstruturaDeRepeticaoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            while (x >= 0.0)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.WriteLine("Digite um numero: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Número negativo!");
+            Console.Read();
+
+        }
+    }
+}
