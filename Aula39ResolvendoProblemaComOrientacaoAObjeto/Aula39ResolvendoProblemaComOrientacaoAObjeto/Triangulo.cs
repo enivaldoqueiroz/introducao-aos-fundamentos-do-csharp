d52e7cf789a4d34d3d03323166c632251545717a
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-24 13:02:21 -0300

    Implementação da solução da area de um triangulo com a criação da classe Triangulo

---
+++ Aula39ResolvendoProblemaComOrientacaoAObjeto/Aula39ResolvendoProblemaComOrientacaoAObjeto/Triangulo.cs
@@ -0,0 +1,10 @@
+﻿namespace Aula39ResolvendoProblemaComOrientacaoAObjeto
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+    }
+}
