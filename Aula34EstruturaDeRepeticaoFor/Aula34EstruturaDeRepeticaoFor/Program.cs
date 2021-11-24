bf2e69430b435408220ac9b09ebb4dc57b86372a
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-24 11:47:41 -0300

    Trabalando com a estrutura de repetição For

---
+++ Aula34EstruturaDeRepeticaoFor/Aula34EstruturaDeRepeticaoFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula34EstruturaDeRepeticaoFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos numeros inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Valor #{i}:");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+            Console.Read();
+        }
+    }
+}
