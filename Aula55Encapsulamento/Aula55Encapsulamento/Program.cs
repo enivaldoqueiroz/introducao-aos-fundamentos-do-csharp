cb8cac5abd666b139d2672fd72b731fc7f3f7d88
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-25 12:33:52 -0300

    Trabalhando com o conseito de encapsulamento

---
+++ Aula55Encapsulamento/Aula55Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula55Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500, 10);
+
+            Console.WriteLine(p.GetNome());
+
+            p.SetNome("TV 4K");
+
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
+
+            Console.Read();
+        }
+    }
+}
