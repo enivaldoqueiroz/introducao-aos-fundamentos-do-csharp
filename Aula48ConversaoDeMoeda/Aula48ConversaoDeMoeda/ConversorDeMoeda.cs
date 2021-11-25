c1026dc7cf7dcebf61c746da743699f59675689a
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-25 10:31:44 -0300

    Implementação do conversor de moedas utilizando os conseitos de membros estaticos em uma classe

---
+++ Aula48ConversaoDeMoeda/Aula48ConversaoDeMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula48ConversaoDeMoeda
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+        
+        public static double ConversaoEmReais(double cotacao, double quantidade)
+        {
+            double conversaoDeDolarParaReal = cotacao * quantidade;
+            double descontoDoIof = (conversaoDeDolarParaReal * Iof) / 100;
+            
+            return conversaoDeDolarParaReal + descontoDoIof;
+        }
+                
+    }
+}
---
+++ Aula48ConversaoDeMoeda/Aula48ConversaoDeMoeda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula48ConversaoDeMoeda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /**Execício de fixação 
+             * 
+             * Faça um programa para ler a cotação do dolar, e depois um valor em dolares
+             * a ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai pagar
+             * pelos dolares, considerando ainda que a pessoa tera que pafar 6% de IOF sobre o valor
+             * em dolar. Criar uma classe ConversorDeMoeda para ser respnsavel pelos calculos
+             * 
+             */
+
+            Console.Write("Qual é a contação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares vc vai comprar? ");
+            double quantidadeDeDolares = double.Parse(Console.ReadLine(), CultureInfo.CurrentCulture);
+
+            double conversaoParaDolares = ConversorDeMoeda.ConversaoEmReais(cotacao, quantidadeDeDolares);
+            Console.Write("Valor a ser pago em reais = " + conversaoParaDolares.ToString("F2", CultureInfo.CurrentCulture));
+
+            Console.Read();
+        }
+    }
+}
