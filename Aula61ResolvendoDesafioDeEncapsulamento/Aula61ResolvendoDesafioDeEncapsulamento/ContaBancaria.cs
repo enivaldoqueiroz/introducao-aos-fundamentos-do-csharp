7f07ccbd54952bf6708365d7c007b3950e22b3d1
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-25 14:42:29 -0300

    Implementação finalizada do desafio

---
+++ Aula61ResolvendoDesafioDeEncapsulamento/Aula61ResolvendoDesafioDeEncapsulamento/ContaBancaria.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula61ResolvendoDesafioDeEncapsulamento
+{
+    internal class ContaBancaria
+    {
+        private double _saldo;
+        private char _simOuNao;
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+
+        public ContaBancaria()
+        {
+            _saldo = 0;
+        }
+        public ContaBancaria(double saldo, char simOuNao) : this()
+        {
+            _saldo = saldo;
+            _simOuNao = simOuNao;
+        }
+
+        public void SimoOuNão(char simOuNao)
+        {
+            if (simOuNao == 's')
+                Desposito();
+                            
+        }
+
+        public void Desposito()
+        {
+            Console.Write("Entre o valor de depósito inicial: ");
+            int valorDepositado = int.Parse(Console.ReadLine());
+             _saldo = _saldo + valorDepositado;
+        }
+
+        public double Saque(double valorSaque)
+        {
+            return _saldo - valorSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + " Saldo: "
+                + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Aula61ResolvendoDesafioDeEncapsulamento/Aula61ResolvendoDesafioDeEncapsulamento/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula61ResolvendoDesafioDeEncapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria c1 = new ContaBancaria();
+
+            Console.Write("Entre o numero da conta: ");
+            int numeroConta = int.Parse(Console.ReadLine());
+
+            c1.Numero = numeroConta;
+
+            Console.Write("Entre o titular da conta: ");
+            string nomeTitular = Console.ReadLine();
+
+            c1.Titular = nomeTitular;
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char simOuNao = char.Parse(Console.ReadLine());
+
+            c1.SimoOuNão(simOuNao);
+
+            Console.WriteLine(c1.ToString());
+
+            Console.Read();
+        }
+    }
+}
