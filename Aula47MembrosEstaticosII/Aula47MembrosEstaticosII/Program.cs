ea0d188b0787bf0f4796252c803498600f268b22
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-25 09:42:49 -0300

    Implementando a classe Calculadora com atributos e metodos estaticos.

---
+++ Aula47MembrosEstaticosII/Aula47MembrosEstaticosII/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula47MembrosEstaticosII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculadora calc = new Calculadora();
+
+            Console.Write("Entre com o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculadora.Circunferencia(raio);
+            double valume = Calculadora.Volume(raio);
+            Console.WriteLine("Circunferencia: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + valume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de PI: " + Calculadora.PI.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Read();
+        }
+    }
+}
