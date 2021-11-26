0db773979f2abae579fe270aaa5e7094696a5792
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-26 15:29:23 -0300

    Trabalhando com moddificar de parametro params

---
+++ Aula72ModificadorDeParamentroParams/Aula72ModificadorDeParamentroParams/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula72ModificadorDeParamentroParams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Calculator.Sum(10, 20, 30, 40);
+            Console.WriteLine(result);
+
+            Console.Read();
+        }
+    }
+}
