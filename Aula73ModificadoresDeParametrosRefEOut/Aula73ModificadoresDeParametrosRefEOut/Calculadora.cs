46ef93886212b0130cb8a7ab84812573a3badb2a
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-26 15:51:46 -0300

    Trabalhando com modificadores ref e out

---
+++ Aula73ModificadoresDeParametrosRefEOut/Aula73ModificadoresDeParametrosRefEOut/Calculadora.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula73ModificadoresDeParametrosRefEOut
+{
+    internal class Calculadora
+    {
+        public static void Triple1(int x)
+        {
+            x *= 3;
+        }
+
+        //Usando o modificador de parametro ref para referencias a variavel original
+        public static void Triple2(ref int x)
+        {
+            x *= 3;
+        }
+        
+        //Usando o modificador de parametro out para referencias a variavel original
+        public static void Triple3(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+
+
+
+
+    }
+}
---
+++ Aula73ModificadoresDeParametrosRefEOut/Aula73ModificadoresDeParametrosRefEOut/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula73ModificadoresDeParametrosRefEOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+
+            //Sem o uso de modificador, a tri
+            Calculadora.Triple1(a);
+
+            //Usando o modificador de parametro ref para referencias a variavel original
+            int b = 10;
+            Calculadora.Triple2(ref b);
+            Console.WriteLine(b);
+
+            //Usando o modificador de parametro out para referencias a variavel original
+            int c = 10;
+            int triple;
+            Calculadora.Triple3(c, out triple);
+            Console.WriteLine(triple);
+
+
+
+            Console.Read();
+        }
+    }
+}
