00f615d3285bdec4d62f55985e92e109efe0d5e0
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-26 14:01:41 -0300

    Execercicio com vetores

---
+++ Aula70VetoresII/Aula70VetoresII/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula70VetoresII
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Aula70VetoresII/Aula70VetoresII/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula70VetoresII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de indice do vertor");
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Digite o nome e o proço do produto: ");
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price};
+            }
+            double soma = 0.0;
+
+            for (int i = 0;i < n; i++)
+            {
+                soma += vect[i].Price;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine("Average price = " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.Read();
+        }
+    }
+}
