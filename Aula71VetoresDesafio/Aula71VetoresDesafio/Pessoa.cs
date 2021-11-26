34315a6dda255a1e040902c8f6acc5aa42be956f
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-26 14:50:06 -0300

    Implementação do desafio com vetores

---
+++ Aula71VetoresDesafio/Aula71VetoresDesafio/Pessoa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula71VetoresDesafio
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Pessoa(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+              
+
+        public override string ToString()
+        {
+            return Nome
+                + ", "
+                + Email;
+        }
+    }
+}
---
+++ Aula71VetoresDesafio/Aula71VetoresDesafio/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula71VetoresDesafio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] vect = new Pessoa[10];
+
+            Console.Write("How many rooms will rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Rent #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Roon: ");
+                int room = int.Parse(Console.ReadLine());
+
+                vect[room] = new Pessoa ( nome, email );
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0;i < vect.Length; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+
+            /*foreach (var i in vect)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            Console.Read();
+        }
+    }
+}
+
