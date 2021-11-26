23c8b65a53cce9d93db98acd7cf1c43fad5da428
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-26 15:59:35 -0300

    Trabalhando com foreach

---
+++ Aula75Foreach/Aula75Foreach/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula75Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] {"Maria", "Alex","Bob" };
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(vect[i]);
+            }
+
+            Console.WriteLine("-----------------------------");
+
+            foreach (string i in vect)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
