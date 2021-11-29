f1f252ed0d02de069ed2a0efd849555d0e7ac27b
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-29 13:38:55 -0300

    Trabalhando com matrize bidimencional

---
+++ Aula79Matrizes/Aula79Matrizes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula79Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Matriz mat de 2 linhas e 3 colunas
+            double[,] mat = new double[2, 3];
+
+            //Verifica o tamanho com Length
+            Console.WriteLine(mat.Length);
+            //Verifica a quantidade de linhas Rank
+            Console.WriteLine(mat.Rank);
+
+            //Verifica a dimenção da matriz com GetLength
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
+
+            Console.ReadLine();
+        }
+    }
+}
