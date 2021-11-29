9ee9bbf1e77969a3c76bd1c043326075b089b8db
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-11-29 13:23:31 -0300

    Exercicio de fixação de lista

---
+++ Aula78ExecicioDeFixacao(Listas)/Aula78ExecicioDeFixacao(Listas)/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula78ExecicioDeFixacao_Listas_
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Salary { get; set; }
+        public Employee(int id, string nome, double salary)
+        {
+            Id = id;
+            Nome = nome;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double porcentage)
+        {
+            Salary = (Salary * (porcentage/100)) + Salary;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + " "
+                + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Aula78ExecicioDeFixacao(Listas)/Aula78ExecicioDeFixacao(Listas)/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula78ExecicioDeFixacao_Listas_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many employees will be registered? ");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("Enter the employee id that will have salary increase : ");
+            int searchId = int.Parse(Console.ReadLine());
+
+            Employee employee = list.Find(x => x.Id == searchId);
+            if (employee != null)
+            {
+                Console.WriteLine("Enter ther porcentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                employee.IncreaseSalary(percentage);
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employess: ");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+            Console.ReadLine();
+
+        }
+        
+    }
+}
