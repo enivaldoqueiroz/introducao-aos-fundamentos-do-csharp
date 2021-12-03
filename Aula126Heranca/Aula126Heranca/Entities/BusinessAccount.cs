d7d0a30de3d4142dc0dae6a07337f8f8664b4345
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-12-03 14:42:49 -0300

    Implementação final da Aula 126

---
+++ Aula126Heranca/Aula126Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula126Heranca.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance) // Recebe os atributos da classe herdada
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
---
+++ Aula126Heranca/Aula126Heranca/Program.cs
@@ -0,0 +1,22 @@
+﻿using Aula126Heranca.Entities;
+using System;
+
+namespace Aula126Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aula 126");
+            Console.WriteLine();
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+
+            Console.WriteLine(account.Balance);
+
+            //A propriedade ou indexador 'propriedade / indexador'
+            //não pode ser usado neste contexto porque o acessador definido está inacessível
+            //account.Balance = 200.00; //O atributo Balance esta protect na classe Account
+
+        }
+    }
+}
