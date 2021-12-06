52793cbcee6f70f48d1ad70e1104e3d93be47d98
enivaldo queiroz <enivaldo.jecq@gmail.com>
2021-12-06 10:05:35 -0300

    Aula 127 Upcasting e downcasting

---
+++ Aula126Heranca/Aula126Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula126Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterstRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number,holder, balance)
+        {
+            InterstRate = interestRate;
+        }
+
+        public void UpdateBalence()
+        {
+            Balance += Balance * InterstRate;
+        }
+
+    }
+}
--- Aula126Heranca/Aula126Heranca/Program.cs
+++ Aula126Heranca/Aula126Heranca/Program.cs
@@ -7,15 +7,47 @@ namespace Aula126Heranca
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Aula 126");
-            Console.WriteLine();
-            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            //Console.WriteLine("Aula 126");
+            //Console.WriteLine();
+            //BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
-            Console.WriteLine(account.Balance);
+            //Console.WriteLine(account.Balance);
 
             //A propriedade ou indexador 'propriedade / indexador'
             //não pode ser usado neste contexto porque o acessador definido está inacessível
             //account.Balance = 200.00; //O atributo Balance esta protect na classe Account
+
+            //Aula127Upcasting e downcasting
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002,"Maria", 0.0, 500.0);
+
+            //UPCASTING
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1003, "Bob", 0.0, 200.0);
+
+            //DOWNCASTING
+
+            //BusinessAccount acc4 = (BusinessAccount)acc2;
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                //Outra forma de aplicar o casting
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                
+                acc5.Loan(200.0);
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                //SavingsAccount acc5 = (SavingsAccount)acc3;
+
+                SavingsAccount acc5 = acc3 as SavingsAccount;
+                acc5.UpdateBalence();
+                Console.WriteLine("Update!");
+            }
+
 
         }
     }
