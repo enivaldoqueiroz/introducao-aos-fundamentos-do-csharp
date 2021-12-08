59a5e48ff5f9a5f600d1129bcb094dce65ab7112
enivaldo queiroz <jose.costa@fitbank.com.br>
2021-12-08 13:26:11 -0300

    Implementação das aulas 133 e 141

---
+++ Aula141EstruturaTry-Catch/Aula141EstruturaTry-Catch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula141EstruturaTry_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aula 141. Estrutura try-catch
+
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("DIvision by zero is not allowed");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error! " + e.Message);
+            }
+
+        }
+    }
+}
